ebb9315ebb52ea190709636cf5d2a593580a94fe
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-11-21 09:22:53 +0200

    Implemented basic server API

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { sample_food } from "./data";
+import { sample_food, sample_tags } from "./data";
 
 const port = 3000;
 
@@ -15,6 +15,33 @@ app.use(
 
 app.get("/api/foods", (req, res) => {
   res.send(sample_food);
+});
+
+app.get("/api/foods/search/:query", (req, res) => {
+  const query = req.params.query;
+  const foods = sample_food.filter((food) =>
+    food.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  res.send(foods);
+});
+
+app.get("/api/foods/tags", (req, res) => {
+  res.send(sample_tags);
+});
+
+app.get("/api/foods/tag/:tag", (req, res) => {
+  const tag = req.params.tag;
+  const foods = sample_food.filter((food) => food?.tags.includes(tag));
+
+  res.send(foods);
+});
+
+app.get("/api/foods/:foodId", (req, res) => {
+  const foodId = req.params.foodId;
+  const food = sample_food.find((food) => food.id == foodId);
+
+  res.send(food);
 });
 
 app.listen(port, () => {
