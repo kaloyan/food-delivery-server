f9fa2d77d5b8251b7e9a018829f10cd5ea511d32
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-11-22 19:34:27 +0200

    Database models
    Implemented food and user database models

---
+++ src/models/food.model.ts
@@ -0,0 +1,63 @@
+import { Schema, model } from "mongoose";
+
+export interface Food {
+  id: string;
+  name: string;
+  price: number;
+  tags: string[];
+  favorite: boolean;
+  stars: number;
+  imageUrl: string;
+  origins: string[];
+  cookTime: string;
+}
+
+export const FoodSchema = new Schema<Food>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    stars: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    origins: {
+      type: [String],
+      required: true,
+      default: [],
+    },
+    cookTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+export const FoodModel = model<Food>("food", FoodSchema);
