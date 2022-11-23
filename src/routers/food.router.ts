e9a6c13977a7f3b688a95a53d2554ad0b9ea1c99
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-11-23 10:37:19 +0200

    Refactored food router to fetch data from database

--- src/routers/food.router.ts
+++ src/routers/food.router.ts
@@ -1,36 +1,70 @@
 import { Router } from "express";
-import { sample_food, sample_tags } from "../data";
+import { sample_food } from "../data";
+import { FoodModel } from "../models/food.model";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send(sample_food);
+router.get("/seed", async (req, res) => {
+  const foodsCount = await FoodModel.countDocuments();
+
+  if (foodsCount > 0) {
+    res.send("Seed is already done.");
+    return;
+  }
+
+  await FoodModel.create(sample_food);
+  res.send("Seed is done.");
 });
 
-router.get("/search/:query", (req, res) => {
-  const query = req.params.query;
-  const foods = sample_food.filter((food) =>
-    food.name.toLowerCase().includes(query.toLowerCase())
-  );
+router.get("/", async (req, res) => {
+  const foods = await FoodModel.find();
+  res.send(foods);
+});
+
+router.get("/search/:query", async (req, res) => {
+  const searchPat = new RegExp(req.params.query, "i");
+  const foods = await FoodModel.find({ name: { $regex: searchPat } });
 
   res.send(foods);
 });
 
-router.get("/tags", (req, res) => {
-  res.send(sample_tags);
+router.get("/tags", async (req, res) => {
+  const tags = await FoodModel.aggregate([
+    {
+      $unwind: "$tags",
+    },
+    {
+      $group: {
+        _id: "$tags",
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$_id",
+        count: "$count",
+      },
+    },
+  ]).sort({ count: -1 });
+
+  const all = {
+    name: "All",
+    count: await FoodModel.countDocuments(),
+  };
+
+  tags.unshift(all);
+
+  res.send(tags);
 });
 
-router.get("/tag/:tag", (req, res) => {
-  const tag = req.params.tag;
-  const foods = sample_food.filter((food) => food?.tags.includes(tag));
-
+router.get("/tag/:tag", async (req, res) => {
+  const foods = await FoodModel.find({ tags: req.params.tag });
   res.send(foods);
 });
 
-router.get("/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_food.find((food) => food.id == foodId);
-
+router.get("/:foodId", async (req, res) => {
+  const food = await FoodModel.findById(req.params.foodId);
   res.send(food);
 });
 
