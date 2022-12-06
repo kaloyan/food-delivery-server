62c886262d16619efdaf19fc9c99a85ebe289130
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-12-06 11:07:39 +0200

    Added tags pagination in food router

--- src/routers/food.router.ts
+++ src/routers/food.router.ts
@@ -3,6 +3,7 @@ import { sample_food } from "../sample-food";
 import { FoodModel } from "../models/food.model";
 
 const router = Router();
+const PAGE_SIZE = 8;
 
 router.get("/seed", async (req, res) => {
   const foodsCount = await FoodModel.countDocuments();
@@ -17,17 +18,66 @@ router.get("/seed", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
-  const foods = await FoodModel.find().limit(10).skip(0);
-  res.send(foods);
+  let query = FoodModel.find({});
+  const qc: any = query.toConstructor();
+
+  const output = {
+    items: await qc().limit(PAGE_SIZE).skip(0),
+    total: await qc().count(),
+    slice: { start: 1, count: PAGE_SIZE },
+  };
+
+  res.send(output);
+});
+
+router.get("/page/:num", async (req, res) => {
+  const offset = Number(req.params.num) || 0;
+
+  let query = FoodModel.find({});
+  const qc: any = query.toConstructor();
+
+  const output = {
+    items: await qc()
+      .limit(PAGE_SIZE)
+      .skip(offset * PAGE_SIZE),
+    total: await qc().count(),
+    slice: { start: offset, count: PAGE_SIZE },
+  };
+
+  res.send(output);
+});
+
+router.get("/search/:query/page/:num", async (req, res) => {
+  const searchPat = new RegExp(req.params.query, "i");
+  const pageNum = Number(req.params.num);
+
+  let query = FoodModel.find({ name: { $regex: searchPat } });
+  const qc: any = query.toConstructor();
+
+  const output = {
+    items: await qc()
+      .limit(PAGE_SIZE)
+      .skip(pageNum * PAGE_SIZE),
+    total: await qc().count(),
+    slice: { start: pageNum, count: PAGE_SIZE },
+  };
+
+  res.send(output);
 });
 
 router.get("/search/:query", async (req, res) => {
   const searchPat = new RegExp(req.params.query, "i");
-  const foods = await FoodModel.find({ name: { $regex: searchPat } })
-    .limit(10)
-    .skip(0);
 
-  res.send(foods);
+  let query = FoodModel.find({ name: { $regex: searchPat } });
+  const qc: any = query.toConstructor();
+
+  const output = {
+    items: await qc().limit(PAGE_SIZE).skip(0),
+    total: await qc().count(),
+    slice: { start: 1, count: PAGE_SIZE },
+  };
+
+  res.send(output);
 });
 
 router.get("/tags", async (req, res) => {
@@ -60,11 +110,36 @@ router.get("/tags", async (req, res) => {
   res.send(tags);
 });
 
+router.get("/tag/:tag/page/:num", async (req, res) => {
+  const tag = req.params.tag;
+  const offset = Number(req.params.num) || 0;
+
+  let query = FoodModel.find({ tags: tag });
+  const qc: any = query.toConstructor();
+
+  const output = {
+    items: await qc()
+      .limit(PAGE_SIZE)
+      .skip(offset * PAGE_SIZE),
+    total: await qc().count(),
+    slice: { start: offset, count: PAGE_SIZE },
+  };
+
+  res.send(output);
+});
+
 router.get("/tag/:tag", async (req, res) => {
-  const foods = await FoodModel.find({ tags: req.params.tag })
-    .limit(10)
-    .skip(0);
-  res.send(foods);
+  const tag = req.params.tag;
+  let query = FoodModel.find({ tags: tag });
+  const qc: any = query.toConstructor();
+
+  const output = {
+    items: await qc().limit(PAGE_SIZE).skip(0),
+    total: await qc().count(),
+    slice: { start: 1, count: PAGE_SIZE },
+  };
+
+  res.send(output);
 });
 
 router.get("/:foodId", async (req, res) => {
